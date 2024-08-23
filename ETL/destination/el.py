f798d8eb5d08fec51bc9dd708dcfe29ea3dfe6ca
MrPotatorust <bumko96@gmail.com>
2024-08-23 18:26:55 +0200

    Finished

--- ETL/destination/el.py
+++ ETL/destination/el.py
@@ -2,7 +2,7 @@ import time
 from time import perf_counter
 import logging
 
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text, MetaData, Table, Column, Integer, String
 
 
 
@@ -13,9 +13,27 @@ staging_engine = create_engine('postgresql://admin:password@staging_database:543
 destination_engine = create_engine('postgresql://admin:password@destination_database:5432/destination_db')
 
 
+# Define the movie_name table
+metadata = MetaData()
+movie_name = Table('movie_name', metadata,
+    Column('id', Integer, primary_key=True),
+    Column('name', String)
+)
+
 with staging_engine.connect() as staging_conn, destination_engine.connect() as destination_conn:
-    movie_name = staging_conn.execute(text('SELECT * FROM movie_names'))
-    logger.info(movie_name.all())
-    logger.info(movie_name)
+    # Fetch data from staging
+    result = staging_conn.execute(text('SELECT * FROM movie_names'))
+    movies = result.fetchall()
+    
+    # Create the table in the destination database
+    metadata.create_all(destination_engine)
+    
+    # Insert data into the destination database
+    for movie in movies:
+        destination_conn.execute(
+            movie_name.insert().values(name=movie[0])
+        )
+    
+    destination_conn.commit()
 
-
+    logger.info(f"Inserted {len(movies)} movies into destination database")
